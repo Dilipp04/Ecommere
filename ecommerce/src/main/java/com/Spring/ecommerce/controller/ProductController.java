278be75e66ce574857654be9ca9b3a4d22ad5d7d
Dilip Prajapati <dilipp11082004@gmail.com>
2026-02-23 17:43:51 +0530

    Implement product update and delete functionality

--- ecommerce/src/main/java/com/Spring/ecommerce/controller/ProductController.java
+++ ecommerce/src/main/java/com/Spring/ecommerce/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 // import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @CrossOrigin
@@ -66,5 +69,35 @@ public class ProductController {
         return ResponseEntity.ok()
                 // .contentType(MediaType.valueOf(product.getImageType()))
                 .body(imageFile);
+    }
+
+    @PutMapping("product/{id}")
+    public ResponseEntity<String> updateProduct(
+            @PathVariable int id,
+            @RequestPart("product") Product product,
+            @RequestPart("imageFile") MultipartFile imageFile) {
+        Product product1 = null;
+        try {
+
+            product1 = service.updateProduct(id, product, imageFile);
+        } catch (IOException e) {
+            return new ResponseEntity<>("Failed to Update", HttpStatus.BAD_REQUEST);
+        }
+        if (product1 != null) {
+            return new ResponseEntity<>("Updated", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Failed to Update", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id) {
+        Product product = service.getProductById(id);
+        if (product != null) {
+            service.deleteProduct(id);
+            return new ResponseEntity<>("Deleted", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Product not Found", HttpStatus.NOT_FOUND);
+        }
     }
 }
